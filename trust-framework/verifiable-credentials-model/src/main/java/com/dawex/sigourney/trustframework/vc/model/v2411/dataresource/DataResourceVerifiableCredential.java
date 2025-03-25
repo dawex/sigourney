928f5aa2ba0943a7f3c3be2084a38277480c2a69
Sébastien Diter <sebastien.diter@dawex.com>
2025-03-25 11:53:26 +0100

    Add embedded context

--- trust-framework/verifiable-credentials-model/src/main/java/com/dawex/sigourney/trustframework/vc/model/v2411/dataresource/DataResourceVerifiableCredential.java
+++ trust-framework/verifiable-credentials-model/src/main/java/com/dawex/sigourney/trustframework/vc/model/v2411/dataresource/DataResourceVerifiableCredential.java
@@ -1,5 +1,7 @@
 package com.dawex.sigourney.trustframework.vc.model.v2411.dataresource;
 
+import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdContexts;
+import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdEmbeddedContext;
 import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdProperty;
 import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdType;
 import com.dawex.sigourney.trustframework.vc.core.vc.v2.model.BaseVerifiableCredential;
@@ -7,11 +9,27 @@ import com.dawex.sigourney.trustframework.vc.core.vc.v2.model.BaseVerifiableCred
 
 import java.time.ZonedDateTime;
 
+import static com.dawex.sigourney.trustframework.vc.core.jsonld.ExternalContext.GAIAX_DEVELOPMENT;
+import static com.dawex.sigourney.trustframework.vc.core.jsonld.ExternalContext.VERIFIABLE_CREDENTIALS_V2;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.SCHEMA_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.SCHEMA_NS;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.VCARD_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.VCARD_NS;
 import static com.dawex.sigourney.trustframework.vc.model.v2411.serialization.Format.DATA_RESOURCE_VERIFIABLE_CREDENTIAL;
 
 /**
  * @see <a href="https://docs.gaia-x.eu/ontology/development/classes/DataResource/">Gaia-X Service Characteristics : DataResource</a>
  */
+@JsonLdContexts(
+		addBaseContext = true,
+		referencedContexts = {
+				VERIFIABLE_CREDENTIALS_V2,
+				GAIAX_DEVELOPMENT
+		},
+		embeddedContexts = {
+				@JsonLdEmbeddedContext(term = SCHEMA_NS, iri = SCHEMA_IRI),
+				@JsonLdEmbeddedContext(term = VCARD_NS, iri = VCARD_IRI),
+		})
 @JsonLdType({"VerifiableCredential", "gx:DataResource"})
 public class DataResourceVerifiableCredential extends BaseVerifiableCredential<DataResourceCredentialSubject> {
 
--- trust-framework/verifiable-credentials-model/src/main/java/com/dawex/sigourney/trustframework/vc/model/v2411/serviceoffering/ServiceOfferingVerifiableCredential.java
+++ trust-framework/verifiable-credentials-model/src/main/java/com/dawex/sigourney/trustframework/vc/model/v2411/serviceoffering/ServiceOfferingVerifiableCredential.java
@@ -1,5 +1,7 @@
 package com.dawex.sigourney.trustframework.vc.model.v2411.serviceoffering;
 
+import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdContexts;
+import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdEmbeddedContext;
 import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdProperty;
 import com.dawex.sigourney.trustframework.vc.core.jsonld.annotation.JsonLdType;
 import com.dawex.sigourney.trustframework.vc.core.vc.v2.model.BaseVerifiableCredential;
@@ -7,11 +9,33 @@ import com.dawex.sigourney.trustframework.vc.core.vc.v2.model.BaseVerifiableCred
 
 import java.time.ZonedDateTime;
 
+import static com.dawex.sigourney.trustframework.vc.core.jsonld.ExternalContext.GAIAX_DEVELOPMENT;
+import static com.dawex.sigourney.trustframework.vc.core.jsonld.ExternalContext.VERIFIABLE_CREDENTIALS_V2;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.DCAT_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.DCAT_NS;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.DC_TERMS_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.DC_TERMS_NS;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.SCHEMA_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.SCHEMA_NS;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.VCARD_IRI;
+import static com.dawex.sigourney.trustframework.vc.model.v2411.Namespace.VCARD_NS;
 import static com.dawex.sigourney.trustframework.vc.model.v2411.serialization.Format.SERVICE_OFFERING_VERIFIABLE_CREDENTIAL;
 
 /**
  * @see <a href="https://docs.gaia-x.eu/ontology/development/classes/ServiceOffering/">Gaia-X Service Characteristics : ServiceOffering</a>
  */
+@JsonLdContexts(
+		addBaseContext = true,
+		referencedContexts = {
+				VERIFIABLE_CREDENTIALS_V2,
+				GAIAX_DEVELOPMENT
+		},
+		embeddedContexts = {
+				@JsonLdEmbeddedContext(term = DCAT_NS, iri = DCAT_IRI),
+				@JsonLdEmbeddedContext(term = DC_TERMS_NS, iri = DC_TERMS_IRI),
+				@JsonLdEmbeddedContext(term = SCHEMA_NS, iri = SCHEMA_IRI),
+				@JsonLdEmbeddedContext(term = VCARD_NS, iri = VCARD_IRI),
+		})
 @JsonLdType({"VerifiableCredential", "gx:ServiceOffering"})
 public class ServiceOfferingVerifiableCredential extends BaseVerifiableCredential<ServiceOfferingCredentialSubject> {
 
